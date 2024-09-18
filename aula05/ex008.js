f636ce0c535e94be7a848f6b0ce72da247bbe2b4
Cleto Silva <168569680+CletoSilva@users.noreply.github.com>
2024-09-18 18:14:33 -0300

    Curso de JavaScript do Curso em Video

---
+++ aula05/ex008.js
@@ -0,0 +1,8 @@
+let velocidade = 76;
+console.log(`A velodidade do seu carro é ${velodidade}Km/h`);
+
+if (velodidade > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+};
+
+console.log(`Dirija sempre usando cinto de segurança!`);
